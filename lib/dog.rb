7ae64974dd577b68fcac4e7d8097cf086440f399
jlew826 <jlew826@gmail.com>
2016-06-13 22:51:33 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+  
+  def initialize(name, breed="Mut")
+    @name=name
+    @breed=breed
+  end
+
+  #def breed=(breed = "Mut")
+  #  @breed=breed
+  #end
+
+  #def breed
+  #  @breed=breed
+  #end
+
+
+
+end
---
+++ lib/person.rb
@@ -0,0 +1,15 @@
+class Person
+  def initialize(name)
+    @name=name
+  end
+
+  #def breed=(breed = "Mut")
+   # @breed=breed
+  
+  #end
+
+  #def breed
+  #  @breed
+  #end
+
+end
